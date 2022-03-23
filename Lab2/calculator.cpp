e1624436c78018ec06bbd4edb97b08ac7fdac0d9
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2022-03-23 16:47:26 -0400

    adding comments

--- Lab2/calculator.cpp
+++ Lab2/calculator.cpp
@@ -8,7 +8,7 @@ struct Calculator{
 
 };
 
-int orderOfOp(char op) {
+int orderOfOp(char op) { //Order of operations determined by number ordered PEMDAS
 	if(op == '^'){
 		return 3;
 	}
@@ -21,7 +21,7 @@ int orderOfOp(char op) {
       	return 0;	
 }
 
-int calcTwo(int a, int b, char op){
+int calcTwo(int a, int b, char op){ //Checks op and then calcs using two int parameters
 	int answer;
 	switch (op) {
 		case '+':
@@ -43,49 +43,50 @@ int calcTwo(int a, int b, char op){
 	return answer;
 }
 
-string changeToPost(string s){
-
-	stack<char> op;
+string changeToPost(string s){ // changing to postfix taking in user input
+	//stack to hold operators
+	stack<char> op; 
 	op.push('!');
+	//string to return 
 	string post= "";
 	string::iterator it;
 
-	for(it = s.begin(); it!=s.end(); it++){
-		if(isalnum(char(*it))){
+	for(it = s.begin(); it!=s.end(); it++){ // iterating through the user input
+		if(isalnum(char(*it))){ //checks for alpha nurmerical at 'it'
 
-         		post = post + *it;
+         		post = post + *it; // appends
 
 		}	
-      		else if(*it == '('){
+      		else if(*it == '('){// checks for open parenthesis first
 
-         		op.push('(');
+         		op.push('(');// if found put into stack
 
 		}
-      		else if(*it == '^'){
+      		else if(*it == '^'){// checks for exponent since its highest after parenthesis
 
          		op.push('^');
 
 		}
-      		else if(*it == ')') {
+      		else if(*it == ')') {// checks for closed 
 
-         		while(op.top() != '!' && op.top() != '(') {
+         		while(op.top() != '!' && op.top() != '(') {// once closed parenthesis is found  it keeps appending to the string until it reaches open parenthesis
            			post = post +" "+ op.top();
             			op.pop();
 
          		}
-         		op.pop();     
+         		op.pop(); // pop the open parenthesis    
       		}
 		else{
 
-         		if(orderOfOp(*it) > orderOfOp(op.top())){
-            			op.push(*it);
-				post=post + " ";
+         		if(orderOfOp(*it) > orderOfOp(op.top())){ // checks for orderOfOp for priority of operations
+            			op.push(*it); //current is higher than top so it gets pushed
+				post=post + " "; 
 
 			}
          		else{
-            			while(op.top() != '!' && orderOfOp(*it) <= orderOfOp(op.top())) {
+            			while(op.top() != '!' && orderOfOp(*it) <= orderOfOp(op.top())) {// checks if current string operation is lower than stack
 
-               				post = post + " " + op.top(); 
+               				post = post + " " + op.top(); //appends
                				op.pop();
 
             			}
@@ -97,75 +98,61 @@ string changeToPost(string s){
       		}
    	}
 
-   	while(op.top() != '!') {
+   	while(op.top() != '!') {// pops out the rest
       		post = post +" "+ op.top();        
       		op.pop();
    	}
 
-   	return post;
+   	return post; // string in postfix
 }
 int calcPost(string postfix){
-
-	stack<int> num;
+	
+	stack<int> num; // int stack used to calculate
 	int answer;
-	num.push(-1);
+	num.push(-1); //push to prevent underflow
+	
 	string equ;
 	string::iterator it;
 
-	for(it = postfix.begin();it!=postfix.end();it++){
-		if(*it == ' ') {
+	for(it = postfix.begin();it!=postfix.end();it++){//iterating through postfix
+		if(*it == ' ') {//skips the whitespace created from changeToPostfix
 			continue;
 		}
-		if(isdigit(*it)){
+		if(isdigit(*it)){// checks if string is num
 
 			int number=0;
-			while(isdigit(*it)){
+			while(isdigit(*it)){// checks next few char if num
 
-				number = number *10 +(int)(*it - '0');
+				number = number *10 +(int)(*it - '0');// casts the string char to int and than add it to the number
 				it++;
 
 			}
 
-			num.push(number);
+			num.push(number); //full number is pushed to stack
 			it--;
 
 		}
-		else{
-
-			int a = num.top();
+		else{// if it hits operator
+			
+			int a = num.top();//pops two numbers to perform operation 
 			num.pop();
 			int b = num.top();
 			num.pop();
-			answer = calcTwo(a,b,*it);
-			/*switch (*it) {
-				case '+':
-					answer = b + a;
-					break;
-				case '-':
-					answer = b - a;
-					break;
-				case '*':
-					answer = b * a;
-					break;
-				case '/':
-					answer = b / a;
-					break;
-				case '^':
-					answer = pow(b , a);
-					break;
-			}*/
-
+			
+			answer = calcTwo(a,b,*it);//checks for which operation to do
 			num.push(answer);
 
 		}
 	}
-	return num.top();
+	return num.top(); //answer is the top of stack
 
 }
 
 int main() {
 	string equation;
 	bool qEnter=true;
+	
+	// until user presses q it loops
 	while(qEnter){
 		cout << "Enter your equation ('q' to exit) : " << endl;
 		cin >> equation;
@@ -174,6 +161,7 @@ int main() {
 			qEnter=false;
 			break;
 		}
+		//output
 		cout << "Equation is " << equation << endl;
 		cout << "Postfix notation: "<<changeToPost(equation) << endl;
 		cout << "Evaluation result: " <<calcPost(changeToPost(equation))<<endl;
